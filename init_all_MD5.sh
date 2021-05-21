f4021d1cca47204c4a9b634519cde50e3b19647c
FPDADA <7366683+FPDADA@user.noreply.gitee.com>
2021-05-21 20:39:00 +0800

    无

---
+++ init_all_MD5.sh
@@ -0,0 +1,7 @@
+    init_data_ID=init_data.sh
+    init_data_MD5=81e775da9a7d600e141c66e830166f7c
+    Util_Functions_ID=Util_Functions.sh
+    Util_Functions_MD5=5cc0074d868e5fcf0702b0379893ae89
+    Cloud_ID=Cloud_Page.zip
+    Cloud_MD5=2f36b8704a9b28e97ec2ee2c5c7d6b00
+    Cloud_Version=2021051919
