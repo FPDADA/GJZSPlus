2878964ddec78c5271c5c84fe0ff9516dcf5d9b8
FPDADA <7366683+FPDADA@user.noreply.gitee.com>
2021-05-22 21:18:37 +0800

    紧急测试

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -4,4 +4,4 @@
     Util_Functions_MD5=5cc0074d868e5fcf0702b0379893ae89
     Cloud_ID=Cloud_Page.zip
     Cloud_MD5=93f78e11d774866643def3e7ec9be2eb
-    Cloud_Version=2021052002
+    Cloud_Version=2021052006
